ff0dd51f728d1f4f318b8bd189d9b135b2e0e5ed
fhugoduarte <h.duarte222@gmail.com>
2019-08-04 22:40:11 -0300

    themeSong finished

--- android/app/src/main/java/com/rnwhoisthispokemon/MainApplication.java
+++ android/app/src/main/java/com/rnwhoisthispokemon/MainApplication.java
@@ -3,6 +3,7 @@ package com.rnwhoisthispokemon;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.rctaudiotoolkit.AudioPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AudioPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage()
       );
--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,10 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { TouchableOpacity } from "react-native";
+import { Player } from "@react-native-community/audio-toolkit";
 
 import { Container, SoundIcon } from "./styles";
 
-export default function Player() {
+const player = new Player(
+  "http://23.237.126.42/soundfiles/gameboy-gbs/pokemon-red/02%20Opening%20%28part%202%29.mp3"
+).play();
+
+export default function CustomPlayer() {
   const [active, setActive] = useState(true);
+
+  useEffect(() => {
+    player.playPause();
+  }, [active]);
 
   return (
     <Container>
